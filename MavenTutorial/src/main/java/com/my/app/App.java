85d737c0ea0464bca57b1c0c09201503e4a4f55e
AnujRawat77 <anuj_rawat@epam.com>
2026-01-13 10:14:16 +0530

    Adding Maven

---
+++ MavenTutorial/src/main/java/com/my/app/App.java
@@ -0,0 +1,15 @@
+package com.my.app;
+
+import com.my.app.helper.Helper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        System.out.println("This is a Maven Project");
+        Helper.help();
+    }
+}
